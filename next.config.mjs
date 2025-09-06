b22485975796407d1c18ae8b52fc26c230e63736
Mebratu <mebratucheka7@gmail.com>
2025-09-06 23:35:10 +0300

    modified: package.json file

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  outputFileTracingRoot: 'C:\\Users\\Mebratu Cheka\\Documents\\MY-PROJECTS\\INSA_Projects\\Group31',
 }
 
 export default nextConfig
